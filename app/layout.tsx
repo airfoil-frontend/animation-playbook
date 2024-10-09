15cbc4e19b51667f4e8c62b7c62e4bd18c5bd6a0
Andre H <andrehadiantolesmana@gmail.com>
2024-10-09 11:35:33 +0800

    ♻️ refactor: init

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,21 +10,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "My Storybook",
-  description: "Next14 + Eslint + Prettier + TailwindCSS",
-  icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "/favicons/icon-light.png",
-        href: "/favicons/icon-light.png",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "/favicons/icon-dark.png",
-        href: "/favicons/icon-dark.png",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
